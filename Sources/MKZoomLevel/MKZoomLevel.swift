6273632157e3014a9b9799a57f135d558511edf8
Dongkyu Kim <stleamist@me.com>
2020-05-18 15:21:09 +0900

    Naive implementation

--- Sources/MKZoomLevel/MKZoomLevel.swift
+++ Sources/MKZoomLevel/MKZoomLevel.swift
@@ -1,3 +1,66 @@
+import MapKit
+
+public extension MKMapView {
+    
+    // MARK: Zoom Level Getter & Setter
+    
+    var zoomLevel: CGFloat {
+        get {
+            let longitudeDeltaAtRightAngle = MKZoomLevel.longitudeDeltaAtRightAngle(of: self.camera, in: self.bounds)
+            return zoomLevel(from: longitudeDeltaAtRightAngle)
+        }
+        set {
+            self.setZoomLevel(newValue, animated: false)
+        }
+    }
+    
+    func setZoomLevel(_ zoomLevel: CGFloat, animated: Bool) {
+        let longitudeDeltaAtRightAngle = longitudeDelta(from: zoomLevel)
+        let longitudeDistanceAtRightAngle = CLLocation(latitude: centerCoordinate.latitude, longitude: longitudeDeltaAtRightAngle).distance(from: CLLocation(latitude: centerCoordinate.latitude, longitude: 0))
+        
+        let region = MKCoordinateRegion(center: centerCoordinate, latitudinalMeters: 0, longitudinalMeters: longitudeDistanceAtRightAngle)
+        
+        let centerCoordinateDistance = MKZoomLevel.centerCoordinateDistanceAtRightAngle(of: region, in: self.bounds)
+        
+        let camera = MKMapCamera(
+            lookingAtCenter: self.camera.centerCoordinate,
+            fromDistance: centerCoordinateDistance,
+            pitch: self.camera.pitch,
+            heading: self.camera.heading
+        )
+        
+        self.setCamera(camera, animated: animated)
+    }
+    
+    // MARK: Unit Conversion
+    
+    private func zoomLevel(from longitudeDelta: CLLocationDegrees) -> CGFloat {
+        return log2(360 * self.frame.size.width / (128 * CGFloat(longitudeDelta)))
+    }
+    
+    private func longitudeDelta(from zoomLevel: CGFloat) -> CLLocationDegrees {
+        return CLLocationDistance(360 * self.frame.size.width / (128 * exp2(zoomLevel)))
+    }
+}
+
+// MARK: Calculation
+
 struct MKZoomLevel {
-    var text = "Hello, World!"
+    
+    static private var computingMapView = MKMapView()
+    static private var lock = NSLock()
+    
+    static func longitudeDeltaAtRightAngle(of camera: MKMapCamera, in bounds: CGRect) -> CLLocationDegrees {
+        lock.lock(); defer { lock.unlock() }
+        computingMapView.bounds = bounds
+        computingMapView.camera = MKMapCamera(lookingAtCenter: camera.centerCoordinate, fromDistance: camera.centerCoordinateDistance, pitch: 0, heading: 0)
+        return computingMapView.region.span.longitudeDelta
+    }
+    
+    static func centerCoordinateDistanceAtRightAngle(of region: MKCoordinateRegion, in bounds: CGRect) -> CLLocationDistance {
+        lock.lock(); defer { lock.unlock() }
+        computingMapView.bounds = bounds
+        computingMapView.region = region
+        return computingMapView.camera.centerCoordinateDistance
+    }
 }
