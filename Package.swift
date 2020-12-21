747a37bd2edb30b6685df4069e7e4677c46f7712
Dongkyu Kim <stleamist@me.com>
2020-12-22 00:00:11 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "MKZoomLevel",
     platforms: [
-        .iOS(.v9), .macOS(.v10_11), .tvOS(.v9)
+        .iOS(.v9), .macOS(.v10_11), .tvOS("9.2")
     ],
     products: [
         .library(
